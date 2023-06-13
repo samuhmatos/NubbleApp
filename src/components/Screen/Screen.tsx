074f53d852b2e2c9460d69b0145d935c4a6bd6c3
samuhmatos <samuhmatos@gmail.com>
2023-06-13 11:44:54 -0300

    Screen Component: Lindando com Safe Area

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Box } from "../Box/Box";
-
+import { useAppSafeArea } from "../../hooks/useAppSafeArea";
 interface ScreenProps{
     children: React.ReactNode
 }
 
 export function Screen({children}:ScreenProps){
+    const {top} = useAppSafeArea()
+
     return (
-        <Box paddingHorizontal="s24">
+        <Box paddingHorizontal="s24" style={{paddingTop: top}}>
             {children}
         </Box>
     )
